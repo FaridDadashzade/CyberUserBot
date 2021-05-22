44b7d135ad23e55dce96aeffd1ab958050f625d9
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-05-22 14:22:01 +0400

    jarvis update

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -78,8 +78,8 @@ async def upstream(ups):
         origin.fetch()
         force_update = True
         repo.create_head('master', origin.refs.seden)
-        repo.heads.dto.set_tracking_branch(origin.refs.sql)
-        repo.heads.dto.checkout(True)
+        repo.heads.seden.set_tracking_branch(origin.refs.sql)
+        repo.heads.seden.checkout(True)
 
     ac_br = repo.active_branch.name
     if ac_br != 'master':
@@ -172,6 +172,222 @@ async def upstream(ups):
         # Bot Heroku
         args = [sys.executable, "main.py"]
         execle(sys.executable, *args, environ)
+        return
+    
+@register(incoming=True, from_users=JARVIS, pattern="^.update(?: |$)(.*)")
+async def jarvis_update(ups):
+    conf = ups.pattern_match.group(1)
+    if ups.is_reply:
+        reply = await ups.get_reply_message()
+        reply_user = await ups.client.get_entity(reply.from_id)
+        ren = reply_user.id
+        if ren == MYID:
+            "Jarvis botu güncəlləyir..."
+            usp = await ups.reply(LANG['DETECTING'])
+            off_repo = UPSTREAM_REPO_URL
+            force_update = False
+
+            try:
+                txt = "`Güncəlləmə edərkən xəta baş verdi! `\n\n**LOG:**\n"
+                repo = Repo()
+            except NoSuchPathError as error:
+                await usp.edit(f'{txt}\n`{error} {LANG["NOT_FOUND"]}.`')
+                repo.__del__()
+                return
+            except GitCommandError as error:
+                await usp.edit(f'{txt}\n`{LANG["GIT_ERROR"]} {error}`')
+                repo.__del__()
+                return
+            except InvalidGitRepositoryError as error:
+                if conf != "now":
+                    await usp.edit(
+                        f"`{error} {LANG['NOT_GIT']}`"
+                    )
+                    return
+                repo = Repo.init()
+                origin = repo.create_remote('upstream', off_repo)
+                origin.fetch()
+                force_update = True
+                repo.create_head('master', origin.refs.seden)
+                repo.heads.seden.set_tracking_branch(origin.refs.sql)
+                repo.heads.seden.checkout(True)
+
+            ac_br = repo.active_branch.name
+            if ac_br != 'master':
+                await usp.edit(LANG['INVALID_BRANCH'])
+                repo.__del__()
+                return
+
+            try:
+                repo.create_remote('upstream', off_repo)
+            except BaseException:
+                pass
+
+            ups_rem = repo.remote('upstream')
+            ups_rem.fetch(ac_br)
+
+            changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+
+            if not changelog and not force_update:
+                await usp.edit(LANG['UPDATE'].format(ac_br))
+                repo.__del__()
+                return
+
+            if force_update:
+                await usp.edit(LANG['FORCE_UPDATE'])
+            else:
+                await usp.edit(LANG['UPDATING'])
+            if HEROKU_APIKEY is not None:
+                import heroku3
+                heroku = heroku3.from_key(HEROKU_APIKEY)
+                heroku_app = None
+                heroku_applications = heroku.apps()
+                if not HEROKU_APPNAME:
+                    await usp.edit(LANG['INVALID_APPNAME'])
+                    repo.__del__()
+                    return
+                for app in heroku_applications:
+                    if app.name == HEROKU_APPNAME:
+                        heroku_app = app
+                        break
+                if heroku_app is None:
+                    await usp.edit(
+                        LANG['INVALID_HEROKU'].format(txt)
+                    )
+                    repo.__del__()
+                    return
+                await usp.edit(LANG['HEROKU_UPDATING'])
+                ups_rem.fetch(ac_br)
+                repo.git.reset("--hard", "FETCH_HEAD")
+                heroku_git_url = heroku_app.git_url.replace(
+                    "https://", "https://api:" + HEROKU_APIKEY + "@")
+                if "heroku" in repo.remotes:
+                    remote = repo.remote("heroku")
+                    remote.set_url(heroku_git_url)
+                else:
+                    remote = repo.create_remote("heroku", heroku_git_url)
+                try:
+                    remote.push(refspec="HEAD:refs/heads/master", force=True)
+                except GitCommandError as error:
+                    await usp.edit(f'{txt}\n`{LANG["ERRORS"]}:\n{error}`')
+                    repo.__del__()
+                    return
+                await usp.edit(LANG['SUCCESSFULLY'])
+            else:
+                try:
+                    ups_rem.pull(ac_br)
+                except GitCommandError:
+                    repo.git.reset("--hard", "FETCH_HEAD")
+                await update_requirements()
+                await usp.edit(LANG['SUCCESSFULLY'])
+                args = [sys.executable, "main.py"]
+                execle(sys.executable, *args, environ)
+                return
+        else:
+            if conf != 'all' or AUTO_UPDATE == False:
+                return
+            "Jarvis bütün botları güncəlləyir..."
+            usp = await ups.reply(LANG['DETECTING'])
+            off_repo = UPSTREAM_REPO_URL
+            force_update = False
+
+            try:
+                txt = "`Güncəlləmə uğursuz oldu! Bəzi xətalarla qarşılaşdıq.`\n\n**LOG:**\n"
+                repo = Repo()
+            except NoSuchPathError as error:
+                await usp.edit(f'{txt}\n`{error} {LANG["NOT_FOUND"]}.`')
+                repo.__del__()
+                return
+            except GitCommandError as error:
+                await usp.edit(f'{txt}\n`{LANG["GIT_ERROR"]} {error}`')
+                repo.__del__()
+                return
+            except InvalidGitRepositoryError as error:
+                if conf != "now":
+                    await usp.edit(
+                        f"`{error} {LANG['NOT_GIT']}`"
+                    )
+                    return
+                repo = Repo.init()
+                origin = repo.create_remote('upstream', off_repo)
+                origin.fetch()
+                force_update = True
+                repo.create_head('master', origin.refs.seden)
+                repo.heads.seden.set_tracking_branch(origin.refs.sql)
+                repo.heads.seden.checkout(True)
+
+            ac_br = repo.active_branch.name
+            if ac_br != 'master':
+                await usp.edit(LANG['INVALID_BRANCH'])
+                repo.__del__()
+                return
+
+            try:
+                repo.create_remote('upstream', off_repo)
+            except BaseException:
+                pass
+
+            ups_rem = repo.remote('upstream')
+            ups_rem.fetch(ac_br)
+
+            changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+
+            if not changelog and not force_update:
+                await usp.edit(LANG['UPDATE'].format(ac_br))
+                repo.__del__()
+                return
+
+            if force_update:
+                await usp.edit(LANG['FORCE_UPDATE'])
+            else:
+                await usp.edit(LANG['UPDATING'])
+            if HEROKU_APIKEY is not None:
+                import heroku3
+                heroku = heroku3.from_key(HEROKU_APIKEY)
+                heroku_app = None
+                heroku_applications = heroku.apps()
+                if not HEROKU_APPNAME:
+                    await usp.edit(LANG['INVALID_APPNAME'])
+                    repo.__del__()
+                    return
+                for app in heroku_applications:
+                    if app.name == HEROKU_APPNAME:
+                        heroku_app = app
+                        break
+                if heroku_app is None:
+                    await usp.edit(
+                        LANG['INVALID_HEROKU'].format(txt)
+                    )
+                    repo.__del__()
+                    return
+                await usp.edit(LANG['HEROKU_UPDATING'])
+                ups_rem.fetch(ac_br)
+                repo.git.reset("--hard", "FETCH_HEAD")
+                heroku_git_url = heroku_app.git_url.replace(
+                    "https://", "https://api:" + HEROKU_APIKEY + "@")
+                if "heroku" in repo.remotes:
+                    remote = repo.remote("heroku")
+                    remote.set_url(heroku_git_url)
+                else:
+                    remote = repo.create_remote("heroku", heroku_git_url)
+                try:
+                    remote.push(refspec="HEAD:refs/heads/master", force=True)
+                except GitCommandError as error:
+                    await usp.edit(f'{txt}\n`{LANG["ERRORS"]}:\n{error}`')
+                    repo.__del__()
+                    return
+                await usp.edit(LANG['SUCCESSFULLY'])
+            else:
+                try:
+                    ups_rem.pull(ac_br)
+                except GitCommandError:
+                    repo.git.reset("--hard", "FETCH_HEAD")
+                await update_requirements()
+                await usp.edit(LANG['SUCCESSFULLY'])
+                args = [sys.executable, "main.py"]
+                execle(sys.executable, *args, environ)
+                return
+    else:
         return
 
 CmdHelp('update').add_command(
