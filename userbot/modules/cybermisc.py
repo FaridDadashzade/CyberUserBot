251f44d3d40451d3b4b04beb702b0a7aab6e06c3
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-06-01 21:38:26 +0400

    cybermisc
    added undelete

--- userbot/modules/cybermisc.py
+++ userbot/modules/cybermisc.py
@@ -4,9 +4,28 @@
 # 
 # oğurlayan peysərdi #
 
+import requests
+import bs4
+import os
+import asyncio
+import time
+import html
+from telethon import *
+from telethon import events
+from telethon.tl import functions
+from datetime import datetime
+from userbot import bot
+from telethon.tl.types import UserStatusEmpty, UserStatusLastMonth, UserStatusLastWeek, UserStatusOffline, UserStatusOnline, UserStatusRecently, ChatBannedRights
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+
+
+import logging
+
 import asyncio
 from userbot.events import register
 from userbot import BOTLOG_CHATID, BOTLOG
+
 
 @register(outgoing=True, incoming=True, func=lambda e: e.mentioned)
 async def ltgm(event):
@@ -46,3 +65,19 @@ async def ltgm(event):
                             event.message,
                             silent=True
                         )
+		
+
+
+@register(outgoing=True, pattern="^.undlt(?: |$)(.*)")
+async def undelete(event):
+    if event.fwd_from:
+        return
+    c = await event.get_chat()
+    if c.admin_rights or c.creator:
+        a = await bot.get_admin_log(event.chat_id, limit=1, search="", edit=False, delete=True)
+        for i in a:
+            await event.reply(i.original.action.message)
+    else:
+        await event.edit("Bu əmri yerinə yetirmək üçün admin olmalısınız!")
+        await asyncio.sleep(3)
+        await event.delete()	
