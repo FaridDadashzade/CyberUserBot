3f1009ac67651fdc2c8cacc94106bacc69c7293e
TheBaghirov <83490137+TheBaghirov@users.noreply.github.com>
2021-06-05 21:43:24 +0400

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -12,6 +12,10 @@ import sys
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
+from userbot import CYBER_VERSION
+import asyncio
+import os
+import requests
 
 # ██████ LANGUAGE CONSTANTS ██████ #
 
@@ -149,6 +153,56 @@ async def raw(event):
             allow_cache=False,
             reply_to=reply_to_id,
             caption="`Həll edilən mesaj`")
+        
+@register(pattern="^.instagram (.*)",outgoing=True)
+async def igza(event):
+    username = event.pattern_match.group(1)
+    last = username.lower()
+    try:
+        await event.edit("`Melumatlar getirilir..`")
+        os.system("pip install instaloader")
+        import instaloader
+        L = instaloader.Instaloader()
+        profile = instaloader.Profile.from_username(L.context, last)
+        pp = profile.get_profile_pic_url()
+        ad = profile.full_name
+        if not ad:
+          ad = "Bu istifadəçinin ad qoymağa ehtiyyacı var."
+        bio = profile.biography
+        if not bio:
+          bio = "Bu istifadəçinin bio qoymağa ehtiyyacı var."
+        follover = profile.followers
+        res = profile.is_verified
+        post = profile.mediacount
+        folloving = profile.followees
+        url = profile.external_url
+        biznes = profile.is_business_account
+        gizli = profile.is_private
+        aydi = profile.userid
+        r = requests.get(pp) 
+        with open("oyeman.jpg", "wb")as file: 
+          file.write(r.content) 
+        igtv = profile.igtvcount
+        msg = f'''
+       **Hesab məlumatları:**
+         **Username:** [{last}](https://instagram.com/{last})
+         **ID:** `{aydi}`
+         **Ad:** `{ad}`
+         **Bio:** `{bio}`
+         **Followers:** `{follover}`
+         **Following:** `{folloving}`
+         **Post sayı:** `{post}`
+         **IgTv sayı:** `{igtv}`
+         **Hesab doğrulanıbmı:** `{res}`
+         **Xarici bağlantı:** `{url}`
+         **Gizli hesabdırmı:** `{gizli}`
+         **Biznes hesabıdırmı:** `{biznes}`
+            '''
+        await event.delete()
+        await event.client.send_file(event.chat_id,"oyeman.jpg",caption=msg)
+        os.remove("oyeman.jpg")
+    except:
+        await event.edit("xəta")
 
 CmdHelp('misc').add_command(
     'random', '<əşya1> <əşya2> ... <şyaN>', 'Əşya listindən təsadufi bir əşya seçər', 'random userator uniborg userge'
@@ -172,4 +226,6 @@ CmdHelp('misc').add_command(
     'resend', None, 'Bir medyayı yenidən göndərər.'
 ).add_command(
     'raw', '<cavab>', 'Cavab verilən mesaj haqqında məlumat verər.'
+).add()
+    'instagram', None, ' Verdiyiniz Instagram hesabı haqqda məlumat verər ⌨️ İşlədilişi: .instagram "accountname"' 
 ).add()
