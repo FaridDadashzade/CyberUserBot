8bdd8a6fa7306c875a90a8512da757e0975fd1f0
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-06-24 23:44:42 +0400

    fix

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -25,6 +25,32 @@ DEFAULTUSER = str(ALIVE_NAME)
 # ============================================
 
 
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["saniyə", "dəqiqə", "saat", "gün"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+
+
 @register(outgoing=True, pattern=r"^\.salive(?: |$)(.*)")
 async def salive(alive):
     user = await bot.get_me()
