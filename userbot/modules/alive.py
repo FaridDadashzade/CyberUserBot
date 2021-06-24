60d1eeba6c1dcd42a2b426633a4ef5b5a2a251c7
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-06-24 23:33:11 +0400

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -1,38 +1,64 @@
 # Copyright 2021 (C) CYBERUSERBOT
+#
+# Farid Dadashzade - CyberUserBot
 
 from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from shutil import which
 from os import remove
-from userbot import ALIVE_NAME, ALIVE_LOGO, bot, CYBER_VERSION
+import time
+
+from userbot import (
+    ALIVE_LOGO,
+    ALIVE_NAME,
+    CYBER_VERSION,
+    StartTime,
+    bot,
+)
+
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
+
 
 # ================= CYBER =================
 DEFAULTUSER = str(ALIVE_NAME)
 # ============================================
 
 
-@register(outgoing=True, pattern="^.salive$")
+@register(outgoing=True, pattern=r"^\.salive(?: |$)(.*)")
 async def salive(alive):
-    """ C Y B Σ R USERBOT """
-    logo = ALIVE_LOGO
-    output = ("**✦ C Y B Σ R USERBOT ✦** \n"
-             f"**✦ Telethon**: `1.17.4` \n"
-             f"**✦ Python**: `3.8.6` \n"
-             f"**✦ Sahibim**: `{DEFAULTUSER}` \n"
-             f"**✦ C Y B Σ R Version**: `{CYBER_VERSION}` \n"
-             f"**✦ Branch**: `Master`")
+    user = await bot.get_me()
+    islememuddeti = await get_readable_time((time.time() - StartTime))
+    kecid = (
+        f"**✦ C Y B Σ R USERBOT ✦** \n"
+        f"┏━━━━━━━━━━━━━━━━━━━━━━━━\n"
+        f"┣[ 🧭 **Botun işləmə müddəti:** `{islememuddeti}`\n"
+        f"┣[ 👤 **Mənim sahibim:** `{DEFAULTUSER}`\n"
+        f"┣[ 🐍 **Python:** `3.8.6`\n"
+        f"┣[ ⚙️ **Telethon:** `1.17.4`\n"
+        f"┣[ 👁‍🗨 **İstifadəçi adı:** @{user.username}\n"
+        f"┣[ 🗄 **Branch:** `Master`\n"
+        f"┗━━━━━━━━━━━━━━━━━━━━━━━━\n"
+        f"**C Y B Σ R:** `{CYBER_VERSION}`"
+    )
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
-            await bot.send_file(alive.chat_id, logo, caption=output)
             await alive.delete()
+            msg = await bot.send_file(alive.chat_id, logo, caption=kecid)
+            await asyncio.sleep(100)
+            await msg.delete()
         except BaseException:
-            await alive.edit(output + "\n\n `Qeyd edilən logo yanlışdır."
-                             "\nXahiş edirəm düzgün bir link qeyd edin.`")
+            await alive.edit(
+                kecid + "\n\n *`Təqdim olunan logo etibarsızdır."
+                "\nKeçidin logo şəklinə yönəldiyindən əmin olun`"
+            )
+            await asyncio.sleep(100)
+            await alive.delete()
     else:
-        await alive.edit(output)            
+        await alive.edit(kecid)
+        await asyncio.sleep(100)
+        await alive.delete()            
         
         
 Help = CmdHelp('salive')
