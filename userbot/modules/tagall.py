34f833a6018f26a2d9d062f7d28e83204998afe4
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-06-25 11:19:38 +0400

    update

--- userbot/modules/tagall.py
+++ userbot/modules/tagall.py
@@ -1,41 +1,35 @@
-# CYBERUSERBOT - Luciferxz #
+# CYBERUSERBOT - FaridDadashzade
+#
+# All rights reserved.
 
-import time
-import requests
 
-from collections import deque
-from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
-from asyncio import sleep
-from random import choice, getrandbits, randint
-from re import sub
-from userbot import CMD_HELP, bot
-from userbot.events import register
-from userbot.modules.admin import get_user_from_event
-from userbot.cmdhelp import CmdHelp
 
-@register(outgoing=True, pattern="^.tagall$")
+from userbot.events import register as cyber
+from userbot.cmdhelp import CmdHelp
+from userbot import bot
+
+
+@cyber(outgoing=True, pattern="^.tagall ?(.*)$")
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "@tag"
+    mentions = event.pattern_match.group(1)
     chat = await event.get_input_chat()
-    leng = 0
-    async for x in bot.iter_participants(chat):
-        if leng < 4092:
-            mentions += f"[\u2063](tg://user?id={x.id})"
-            leng += 1
+    async for x in bot.iter_participants(chat, 150):
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
     await event.reply(mentions)
     await event.delete()
 
-@register(outgoing=True, pattern="^.admin")
+
+@cyber(outgoing=True, pattern="^.admin ?(.*)$")
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "@admin"
+    mentions = "Adminlər: "
     chat = await event.get_input_chat()
     async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f"[\u2063](tg://user?id={x.id})"
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
     reply_message = None
     if event.reply_to_msg_id:
         reply_message = await event.get_reply_message()
@@ -44,8 +38,8 @@ async def _(event):
         await event.reply(mentions)
     await event.delete()
 
-CmdHelp('tagall').add_command(
-    'tagall', None, 'Bu əmri hər hansısa söhbətdə işlədəndə hərkəsi tağ edir.'
+CmdHelp('tags').add_command(
+    'tagall', '<səbəb>', 'Bir mesajda 150 istifadəçini etiketləyər (Maksimum 150)'
 ).add_command(
-    'admin', None, 'Bu əmri hər hansıxa sohbətdə işlədəndə adminləri tağ edər.'
+    'admin', '<səbəb>', 'Qrupdaki adminləri bir mesajda etiketləyər.'
 ).add()
