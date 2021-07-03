6636f32d2f294e604ffc96a4c77bbe8237be49c7
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-07-03 22:29:17 +0400

    update

--- userbot/cybervc/player.py
+++ userbot/cybervc/player.py
@@ -5,3 +5,16 @@
 import asyncio
 import os
 from datetime import datetime, timedelta
+
+# tezlikle
+#
+#
+#
+
+# Pyrogram filterleri
+
+main_filter = filters.group & filters.text & ~filters.edited & ~filters.via_bot
+self_or_contact_filter = filters.create(
+    lambda _, __, message: (message.from_user and message.from_user.is_contact)
+    or message.outgoing
+)
