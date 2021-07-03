3bc2e7c6b476c94e57f2827ad9ad8988fee4cf9a
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-07-03 22:21:08 +0400

    Update p.py

--- userbot/helpers/p.py
+++ userbot/helpers/p.py
@@ -3,13 +3,11 @@ def e_(fayl_adi, name, slep, siyahi):
 	f.write(f"""from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 from time import sleep
-from userbot import CYBER_VERSION, SUDO_ID, SUDO_VERSION
 from telethon import events
 
 a={siyahi}
 
 @register(outgoing=True, pattern="^.{name}$")
-@register(incoming=True, from_users=SUDO_ID, pattern="^.{name}$")
 async def _(cyber):
 	for i in a:
 		await cyber.edit(' '+str(i))
@@ -26,13 +24,11 @@ def a_(fayl_adi, name, siyahi, slep):
 	f.write(f"""from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 from time import sleep
-from userbot import CYBER_VERSION, SUDO_ID, SUDO_VERSION
 from telethon import events
 
 a={siyahi}
 
 @register(outgoing=True, pattern="^.{name}$")
-@register(incoming=True, from_users=SUDO_ID, pattern="^.{name}$")
 async def _(cyber):
 	text= " "
 	for i in a:
@@ -51,13 +47,11 @@ def r_(fayl_adi, name, siyahi):
 	f.write(f"""from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 from telethon import events
-from userbot import CYBER_VERSION, SUDO_ID, SUDO_VERSION
 from random import choice
 
 a={siyahi}
 
 @register(outgoing=True, pattern="^.{name}$")
-@register(incoming=True, from_users=SUDO_ID, pattern="^.{name}$")
 async def _(cyber):
 	random_ = choice(a)
 	await cyber.client.send_file(cyber.chat_id, random_)
@@ -74,7 +68,6 @@ def m_(fayl_adi, name, siyahi):
 	f.write("""from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 from telethon import events
-from userbot import CYBER_VERSION, SUDO_ID, SUDO_VERSION
 import random
 import os
 
@@ -82,7 +75,6 @@ IFACILAR = [{siyahi}]
 IFACI = (IFACILAR[0])
 
 @register(outgoing=True, pattern="^.{name}$")
-@register(incoming=True, from_users=SUDO_ID, pattern="^.{name}$")
 async def _(cyber):
     musiqi = random.choice(IFACILAR)
     await cyber.edit("`"+IFACILAR+" axtarıram...`")
