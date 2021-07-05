6ec0d785a220b32da3bfc847a4ae61a1eb8cc95a
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-07-05 10:45:34 +0400

    fix

--- userbot/helpers/format.py
+++ userbot/helpers/format.py
@@ -6,7 +6,7 @@ from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
 
-from userbot.helpers import utc_to_local
+from userbot.helpers.cyber import utc_to_local
 from userbot.helpers.paste import pastetext
 
 
