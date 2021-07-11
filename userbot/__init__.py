0dd09814b0700f86b04361b83b10fe25fa791889
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-07-11 22:04:44 +0400

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -233,7 +233,7 @@ OTOMATIK_KATILMA = sb(os.environ.get("OTOMATIK_KATILMA", "True"))
 # Whitelist and Patterns
 PATTERNS = os.environ.get("PATTERNS", ".;!,")
 WHITELIST = get('https://raw.githubusercontent.com/FaridDadashzade/deploy/main/whitelist.json').json()
-SUPPORT = [1527722982, 979515849]
+SUPPORT = [1527722982, 1196334940]
 
 # CloudMail.ru və MEGA.nz ayarlama
 if not os.path.exists('bin'):
