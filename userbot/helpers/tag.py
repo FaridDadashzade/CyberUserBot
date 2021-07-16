1ef405d5b49690e5f8b160530a05e26137eb8aa2
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-07-16 15:06:33 +0400

    Update tag.py

--- userbot/helpers/tag.py
+++ userbot/helpers/tag.py
@@ -1,3 +1,8 @@
+# CYBERUSERBOT
+#
+
+
+
 import re
 from random import choice
 import logging
@@ -14,3 +19,48 @@ from telethon.tl.types import (
     MessageEntityTextUrl,
     MessageEntityUnderline,
 )
+
+#------------------ CYBERUSERBOT -------------------#
+
+
+PARSED_ENTITIES = (
+    MessageEntityBold,
+    MessageEntityItalic,
+    MessageEntityCode,
+    MessageEntityPre,
+    MessageEntityTextUrl,
+    MessageEntityUnderline,
+)
+
+
+
+
+async def tag(event):
+    newstr = event.text
+    if event.entities:
+        newstr = nameexp.sub(r'<a href="tg://user?id=\2">\3</a>', newstr, 0)
+        for match in usernexp.finditer(newstr):
+            user = match.group(1)
+            text = match.group(2)
+            name, entities = await event.client._parse_message_text(text, "md")
+            rep = f'<a href="tg://resolve?domain={user}">{name}</a>'
+            if entities:
+                for e in entities:
+                    tag = None
+                    if isinstance(e, types.MessageEntityBold):
+                        tag = "<b>{}</b>"
+                    elif isinstance(e, types.MessageEntityItalic):
+                        tag = "<i>{}</i>"
+                    elif isinstance(e, types.MessageEntityCode):
+                        tag = "<code>{}</code>"
+                    elif isinstance(e, types.MessageEntityStrike):
+                        tag = "<s>{}</s>"
+                    elif isinstance(e, types.MessageEntityPre):
+                        tag = "<pre>{}</pre>"
+                    elif isinstance(e, types.MessageEntityUnderline):
+                        tag = "<u>{}</u>"
+                    if tag:
+                        rep = tag.format(rep)
+            newstr = re.sub(re.escape(match.group(0)), rep, newstr)
+    if newstr != event.text:
+        await event.edit(newstr, parse_mode="html")
